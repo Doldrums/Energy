bb9811164e6e6ede30d3df67ce1f9863ba9f3ce6
doldrums.317@gmail.com <Cheverdaarina24>
2019-02-27 14:46:46 +0200

    add something

--- app/src/main/java/com/example/energy/fragments/UserFragment.kt
+++ app/src/main/java/com/example/energy/fragments/UserFragment.kt
@@ -1,7 +1,7 @@
 package com.example.energy.fragments
 
-import android.content.Context
-import android.net.Uri
+
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -11,11 +11,14 @@ import android.view.ViewGroup
 import com.example.energy.R
 import kotlinx.android.synthetic.main.fragment_user.*
 
+import com.jjoe64.graphview.series.DataPoint
+
+import com.jjoe64.graphview.series.BarGraphSeries
+import com.jjoe64.graphview.series.LineGraphSeries
+
 
 class UserFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,5 +36,54 @@ class UserFragment : Fragment() {
             UserFragment().apply {
 
             }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val series1 = LineGraphSeries<DataPoint>(
+            arrayOf<DataPoint>(
+                DataPoint(0.0, 1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        series1.title = "Random Curve 1";
+        series1.color = Color.GREEN;
+        series1.isDrawDataPoints = true
+        series1.dataPointsRadius = 10F
+        series1.thickness = 8
+        graph1.addSeries(series1)
+        graph1.viewport.isScalable = true
+        graph1.viewport.isScrollable = true
+        graph1.viewport.setScalableY(true)
+        graph1.viewport.setScrollableY(true)
+
+        val series = BarGraphSeries(
+            arrayOf(
+                DataPoint(0.0, -1.0),
+                DataPoint(1.0, 5.0),
+                DataPoint(2.0, 3.0),
+                DataPoint(3.0, 2.0),
+                DataPoint(4.0, 6.0)
+            )
+        )
+        graph.addSeries(series)
+
+
+        series.setValueDependentColor { data ->
+            Color.rgb(
+                data.x.toInt() * 255 / 4,
+                Math.abs(data.y * 255 / 6).toInt(),
+                100
+            )
+        }
+
+        series.spacing = 50
+        series.isDrawValuesOnTop = true
+        series.valuesOnTopColor = Color.RED
+
+
     }
 }
